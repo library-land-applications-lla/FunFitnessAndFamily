692062558e95d706eca1925f895ae0e215840f7b
Jeevan Gyawali <jeewangw9@gmail.com>
2019-07-16 19:27:26 -0500

    Add files via upload

---
+++ src/forest/config/config.php
@@ -0,0 +1,10 @@
+<?php
+    include_once 'config/init.php';
+
+    return array(
+	    'host' => 'localhost',
+	    'db_username' => 'root',
+	    'db_password' => '',
+	    'db_name' => 'animals'
+    );
+?>
